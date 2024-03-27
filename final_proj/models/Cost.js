baa76a7a4b4ca9174c96e7734979c1ae1f05c193
EyalAv1 <eyalavni6@gmail.com>
2024-03-27 14:03:31 +0200

    first commit

---
+++ final_proj/models/Cost.js
@@ -0,0 +1,42 @@
+// Dor Bergel 322271792, Eyal Avni 212211734, Itay Vekselbum 211871827
+// require everything we need to define the cost modal
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// define the cost schema
+const CostSchema = new Schema({
+    user_id: {
+        type: Number
+    },
+    year: {
+        type: Number
+    },
+    month: {
+        type: Number
+    },
+    day: {
+        type: Number
+    },
+    description: {
+        type: String
+    },
+    category: {
+        type: String
+    },
+    sum: {
+        type: Number
+    },
+    id: {
+      type: mongoose.Types.ObjectId,
+      require: true,
+      default: function () {
+          return this._id
+      }
+    }
+}, {versionKey: false});
+
+//create the cost modal
+const Cost = mongoose.model('Costs', CostSchema, 'Costs');
+
+//export the cost modal
+module.exports = Cost;
